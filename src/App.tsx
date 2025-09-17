a213cb0044ea67957619f39628097de09e1b3648
PurpleDNA <kadirimaroof@gmail.com>
2025-09-17 12:03:23 +0100

    I need analytics to work at the moment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Outlet } from "react-router";
-
+import { Analytics } from "@vercel/analytics/next";
 function App() {
   return (
-    <>
+    <div>
       <Outlet />
-    </>
+      <Analytics />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { RouterProvider } from "react-router/dom";
 import Gameweek from "./components/Gameweek.tsx";
 import Home from "./components/Home.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
@@ -32,7 +31,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <Analytics />
     </QueryClientProvider>
   </StrictMode>
 );
